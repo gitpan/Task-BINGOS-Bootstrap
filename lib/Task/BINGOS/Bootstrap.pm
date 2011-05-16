2894cb08050e494836d9ce34e316d5891c1a404f
Chris Williams <chris@bingosnet.co.uk>
2011-05-16 12:24:20 +0000

    Import of BINGOS/Task-BINGOS-Bootstrap-0.02 from CPAN.
    gitpan-cpan-distribution: Task-BINGOS-Bootstrap
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         BINGOS/Task-BINGOS-Bootstrap-0.02.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

---
+++ lib/Task/BINGOS/Bootstrap.pm
@@ -0,0 +1,56 @@
+package Task::BINGOS::Bootstrap;
+BEGIN {
+  $Task::BINGOS::Bootstrap::VERSION = '0.02';
+}
+
+#ABSTRACT: Bootstrap CPANPLUS with cpanm like BINGOS does
+
+use strict;
+use warnings;
+
+qq[smoky smoke box foo]
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Task::BINGOS::Bootstrap - Bootstrap CPANPLUS with cpanm like BINGOS does
+
+=head1 VERSION
+
+version 0.02
+
+=head1 SYNOPSIS
+
+  cpanm Task::BINGOS::Bootstrap
+
+=head1 DESCRIPTION
+
+Task::BINGOS::Bootstrap is a L<Task> that installs all the modules that I need to start using L<CPANPLUS>.
+I use L<App::cpanminus> to do the bootstrapping then switch to L<CPANPLUS>.
+
+The following things will be installed:
+
+  CPANPLUS
+
+  CPANPLUS::Internals::Source::CPANIDX
+
+  Test::Reporter::Transport::Socket
+
+  CPANPLUS::YACSmoke
+
+=head1 AUTHOR
+
+Chris Williams <chris@bingosnet.co.uk>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Chris Williams.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
